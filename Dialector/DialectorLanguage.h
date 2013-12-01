0b4076601a32dbda899f4d576500beb6423627e9
Keith Avery <keith.marshall.avery@gmail.com>
2013-12-01 12:23:29 -0500

    icons

---
+++ Dialector/DialectorLanguage.h
@@ -0,0 +1,13 @@
+//
+//  DialectorLanguage.h
+//  Dialector
+//
+//  Created by Keith Avery on 12/1/13.
+//  Copyright (c) 2013 Keith Avery. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DialectorLanguage : NSObject
+
+@end
---
+++ Dialector/DialectorLanguage.m
@@ -0,0 +1,13 @@
+//
+//  DialectorLanguage.m
+//  Dialector
+//
+//  Created by Keith Avery on 12/1/13.
+//  Copyright (c) 2013 Keith Avery. All rights reserved.
+//
+
+#import "DialectorLanguage.h"
+
+@implementation DialectorLanguage
+
+@end
